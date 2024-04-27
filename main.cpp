0f7febbd05e4288b46079e200509b016ce9adbd9
maximspbu <st119024@student.spbu.ru>
2024-04-27 11:42:31 +0100

    add WriteAllocate function, ReadAllocate function

--- main.cpp
+++ main.cpp
@@ -4,25 +4,41 @@
 #include <cstring>
 #define Gb sizeof(int)*2*128*1024*1024
 
-void Write(){
 
+void ReadAllocate(size_t numGb, size_t step){
+    int *a = (int*)malloc(numGb*step*sizeof(int));
+    size_t i = 0;
+    char c;
+    volatile int r;
+    size_t x = 0;
+    while (i < numGb){
+        x = i*step;
+        while (x < (i + 1)*step){
+            r = a[x];
+            ++x;
+        }
+        r = a[i];
+        ++i;
+        std::cout << "Allocated " << step/(Gb) << " Gb\n";
+        system("free");
+        std::cout << "Continue? y/n\n";
+        std::cin >> c;
+        if (c != 'y'){
+            break;
+        }
+    }
+    free(a);
 }
 
-void Read(){
-
-}
-
-void Allocate(size_t numGb, size_t step){
-    int **a = new int*[numGb];
+void WriteAllocate(size_t numGb, size_t step){
+    int **a = (int**)malloc(step*sizeof(int*));
     size_t i = 0;
     char c;
     while (i < numGb){
-        a[i] = static_cast<int*>(malloc(step));
-        //Write();
-        //Read();
+        a[i] = (int*)malloc(step);
         memset((void*)a[i], 1, step);
         ++i;
-        std::cout << "Allocated " << (int)step/Gb << " Gb\n";
+        std::cout << "Allocated " << step/(Gb) << " Gb\n";
         system("free");
         std::cout << "Continue? y/n\n";
         std::cin >> c;
@@ -31,14 +47,13 @@ void Allocate(size_t numGb, size_t step){
         }
     }
     for (size_t x = 0; x < numGb; ++x){
-        free(a[i]);
+        free((void*)a[i]);
     }
-    delete[] a;
+    free(a);
 }
 
 int main(){
-    // Read();
-    // Write();
-    Allocate(8, Gb); //8Gb will allocate, 1Gb step
+    system("echo 1 | sudo tee /proc/sys/vm/overcommit_memory"); //default value = 0
+    WriteAllocate(8, Gb); //8Gb will allocate, 1Gb step
     return 0;
 }
